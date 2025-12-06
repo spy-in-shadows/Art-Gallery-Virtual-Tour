59aceba58f036dec6bc74b265dac38d7288a736c
Krishna Verma <krishnaias1289@gmail.com>
2025-12-06 23:02:24 +0530

    Frst commit

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from(".logo", {
+  opacity: 0,
+  y: -20,
+  duration: 1
+});
+
+gsap.from(".hero-text", {
+  opacity: 0,
+  y: 50,
+  duration: 1.2
+});
+
+gsap.from(".hero-subtext", {
+  opacity: 0,
+  y: 40,
+  delay: 0.3,
+  duration: 1
+});
